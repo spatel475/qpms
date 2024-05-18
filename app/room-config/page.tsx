6117b951c5752b286e9c75b86358d3ee9b334f8d
Sahil Patel <saheeelpatel@gmail.com>
2024-05-18 10:35:37 -0500

    cleanup room-config page

--- app/room-config/page.tsx
+++ app/room-config/page.tsx
@@ -1,18 +1,17 @@
 "use client";
 
 import { DataTable } from "@/components/data-table/data-table";
-import { Card, CardContent, CardDescription, CardHeader } from "@/components/ui/card";
-
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader } from "@/components/ui/card";
 import { toast } from "@/components/ui/use-toast";
 import { get, put } from "@/lib/fetch";
 import { getCachedData, setCachedData } from "@/lib/memory-cache";
 import { Edit, Save, Undo } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Room } from "../models/models";
 import { CACHE_KEY, roomConfigColumns } from "./columns";
 
-export default function RoomConfig() {
+const RoomConfig = () => {
 	const [isLoading, setIsLoading] = useState(true);
 	const [isEditModeOn, setIsEditModeOn] = useState(false);
 	const [data, setData] = useState<Room[]>([]);
@@ -23,63 +22,66 @@ export default function RoomConfig() {
 		pageSize: 20,
 	});
 
-	const fetchData = async (page: number, pageSize: number) => {
-		setIsLoading(true);
+	const fetchRooms = useCallback(
+		async (page: number, pageSize: number) => {
+			setIsLoading(true);
 
-		const cacheKey = `rooms-page-${page}-size-${pageSize}`;
-		const cachedData = getCachedData(cacheKey);
+			const cacheKey = `rooms-page-${page}-size-${pageSize}`;
+			const cachedData = getCachedData(cacheKey);
 
-		if (cachedData) {
-			setData(cachedData.data);
-			setPagination((prev) => ({
-				...prev,
-				totalCount: cachedData.totalCount,
-				totalPages: cachedData.totalPages,
-				pageSize: cachedData.pageSize,
-				currentPage: page,
-			}));
-			setIsLoading(false);
-		} else {
-			try {
-				const response = await get<Room[]>("/rooms", {
-					queryParams: {
-						page: pagination.currentPage.toString(),
-						limit: pagination.pageSize.toString(),
-					},
-				});
-				const fetchedData = response.data;
-				const totalCount = parseInt(response.headers["x-total-count"], 10);
-				const totalPages = Math.ceil(totalCount / pageSize);
-
-				setData(fetchedData);
+			if (cachedData) {
+				setData(cachedData.data);
 				setPagination((prev) => ({
 					...prev,
-					totalCount,
-					totalPages,
-					pageSize,
+					totalCount: cachedData.totalCount,
+					totalPages: cachedData.totalPages,
+					pageSize: cachedData.pageSize,
 					currentPage: page,
 				}));
-
-				setCachedData(cacheKey, {
-					data: fetchedData,
-					totalCount,
-					totalPages,
-					pageSize,
-				});
-
 				setIsLoading(false);
-			} catch (error) {
-				console.error("Error fetching rooms:", error);
-				setIsLoading(false);
+			} else {
+				try {
+					const response = await get<Room[]>("/rooms", {
+						queryParams: {
+							page: pagination.currentPage.toString(),
+							limit: pagination.pageSize.toString(),
+						},
+					});
+					const fetchedData = response.data;
+					const totalCount = parseInt(response.headers["x-total-count"], 10);
+					const totalPages = Math.ceil(totalCount / pageSize);
+
+					setData(fetchedData);
+					setPagination((prev) => ({
+						...prev,
+						totalCount,
+						totalPages,
+						pageSize,
+						currentPage: page,
+					}));
+
+					setCachedData(cacheKey, {
+						data: fetchedData,
+						totalCount,
+						totalPages,
+						pageSize,
+					});
+
+					setIsLoading(false);
+				} catch (error) {
+					console.error("Error fetching rooms:", error);
+					setIsLoading(false);
+				}
 			}
-		}
-	};
+		},
+		[pagination.currentPage, pagination.pageSize]
+	);
 
 	useEffect(() => {
-		fetchData(pagination.currentPage, pagination.pageSize);
-	}, [pagination.currentPage, pagination.pageSize]);
+		fetchRooms(pagination.currentPage, pagination.pageSize);
+	}, [fetchRooms, pagination.currentPage, pagination.pageSize]);
 
-	const handleUndoChanges = () => {
+	const clearCache = () => {
 		setCachedData(CACHE_KEY, []);
 		setIsEditModeOn(false);
 	};
@@ -117,9 +119,7 @@ export default function RoomConfig() {
 				description: `${updatedRooms.length} rooms updated`,
 			});
 
-			// Clear the cache and turn off edit mode only on successful save
-			setCachedData(CACHE_KEY, []);
-			setIsEditModeOn(false);
+			clearCache();
 		} catch (error) {
 			console.error("Error updating room configurations:", error);
 
@@ -136,12 +136,12 @@ export default function RoomConfig() {
 			<CardHeader className="flex flex-row justify-between">
 				<div>
 					<h1 className="flex-1 shrink-0 whitespace-nowrap text-xl font-semibold tracking-tight sm:grow-0">Room Configuration</h1>
-					<CardDescription>Manage room types, rates, and availabilty. </CardDescription>
+					<CardDescription>Manage room types, rates, and availability.</CardDescription>
 				</div>
 				<div className="edit-cell-container">
 					{isEditModeOn ? (
 						<div className="edit-cell-action">
-							<Button variant="outline" size="icon" name="cancel" onClick={handleUndoChanges}>
+							<Button variant="outline" size="icon" name="cancel" onClick={clearCache}>
 								<Undo className="h-4 w-4" />
 							</Button>{" "}
 							<Button variant="outline" size="icon" name="done" onClick={handleSaveChanges}>
@@ -158,8 +158,10 @@ export default function RoomConfig() {
 				</div>
 			</CardHeader>
 			<CardContent>
-				<DataTable isLoading={isLoading} columns={roomConfigColumns(isEditModeOn)} data={data} pagination={pagination} setPagination={setPagination}></DataTable>
+				<DataTable isLoading={isLoading} columns={roomConfigColumns(isEditModeOn)} data={data} pagination={pagination} setPagination={setPagination} />
 			</CardContent>
 		</Card>
 	);
-}
+};
+
+export default RoomConfig;
