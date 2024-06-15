3ac849adc4660a9645496db61e9b15890f7e5d99
Sahil Patel <saheeelpatel@gmail.com>
2024-06-14 23:23:05 -0500

    protect ui pages

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,6 @@ import { redirect } from "next/navigation";
 import LoginForm from "./form";
 
 export default async function LoginPage() {
-	console.log("in login");
 	const session = await getServerSession();
 	if (session) {
 		redirect("/");
